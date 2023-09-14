a0b4d635e06c22fda2873bf27ee8ce23ec5169b1
mehwishjbe <mehwish.jbe@gmail.com>
2023-09-14 15:26:54 +0200

    variable declaration JS

---
+++ main.js
@@ -0,0 +1,5 @@
+var selectBtn4 = document.getElementById('circle4');
+selectBtn4.addEventListener('click', onClickBtn4);
+
+var selectBtn5 = document.getElementById('circle5');
+selectBtn5.addEventListener('click', onClickBtn5);
