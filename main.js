6ff2e538aa6a51afd2579d98438e90ee273f188f
ad.hanif <ad.hanif@outlook.com>
2023-09-17 14:02:52 +0200

    rating is workign

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-var listItems = document.querySelectorAll('.circle-rank'); // Use '.circle-rank-line' to select elements by class
-listItems.forEach(function(item) {
-  item.addEventListener('click', selectItem);
-  
+//
+//
+//
+//
+// const listItems = document.querySelectorAll(".circle-rank");
+// listItems.forEach(function (item) {
+//   item.addEventListener("click", selectItem);
+//   console.log("it is working");
+// });
+
+// function selectItem() {
+//   const btn5 = document.getElementById("circle5");
+//   this.style.backgroundColor = "hsl(217, 12%, 63%)";
+//   if (this === btn5) {
+//     this.style.backgroundColor = "hsl(25, 97%, 53%)";
+//   }
+// }
+
+const submitButton = document.querySelector(".submit-btn");
+
+const circleElements = document.querySelectorAll(".circle-rank");
+
+let slectedRating = null;
+
+circleElements.forEach(function (ele) {
+  ele.addEventListener("click", function () {
+    ele.style.backgroundColor = "hsl(217, 12%, 63%)";
+    slectedRating = ele.textContent;
+  });
 });
 
-function selectItem() {
-    var btn5 = document.getElementById('circle5')
-    this.style.backgroundColor = 'hsl(217, 12%, 63%)';
-     if(this === btn5){
-        this.style.backgroundColor = 'hsl(25, 97%, 53%)';
-     }
-}
+submitButton.addEventListener("click", function () {
+  if (slectedRating !== null) {
+    const resultDiv = document.querySelector("#result");
+    resultDiv.textContent = "You have selected " + slectedRating + " out of 5";
+  }
+  
+});
